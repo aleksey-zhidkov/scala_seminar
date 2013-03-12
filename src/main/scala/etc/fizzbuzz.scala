9f06fb8bde747b6e5ab3a5762603df6e4caa8fd3
Aleksey Zhidkov <azhidkov@playtox.ru>
2013-03-12 15:20:43 +0700

    fixed again:)

--- src/main/scala/etc/fizzbuzz.scala
+++ src/main/scala/etc/fizzbuzz.scala
@@ -1,6 +1,6 @@
 for (i <- 0 to 100) {
   i match {
-    case fizzbuzz if i % 15 => println("fizzbuzz")
+    case fizzbuzz if i % 15 == 0 => println("fizzbuzz")
     case fuzz if i % 3 == 0 => println("fizz")
     case buzz if i % 5 == 0 => println("buzz")
     case _ => println(i)
