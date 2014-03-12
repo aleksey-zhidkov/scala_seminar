44074718a8c61cce1b3826934ebd1f62d0e8e6fa
Aleksey Zhidkov <aleksey.zhidkov@gmail.com>
2014-03-12 21:32:58 +0700

    interpreter initial commit gradle version upgraded to 1.11 hello_world script

---
+++ interpreter/src/test/scala/azhidkov/ast/ArithmeticNodeSpec.scala
@@ -0,0 +1,47 @@
+package azhidkov.ast
+
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{GivenWhenThen, Matchers, WordSpec}
+import org.scalatest.prop.TableDrivenPropertyChecks._
+
+@RunWith(classOf[JUnitRunner])
+class ArithmeticNodeSpec extends WordSpec with Matchers with GivenWhenThen {
+
+  val argumentsList =
+    Table(("x", "y", "z", "res"),
+      (2, 1, 3, 6),
+      (0, 1, 3, 2),
+      (0, 5, 3, -2),
+      (0, 0, 0, 0)
+    )
+
+  forAll(argumentsList) {
+    (x: Int, y: Int, z: Int, res: Int) =>
+
+      s"An expression of x*x - y + z with values ($x, $y, $z)" when {
+
+        "evaluated with arithmetic nodes" should {
+
+          s"return $res" in {
+
+            Given(s"Tree for expression")
+            val node = PlusNode(
+              MinusNode(
+                MultiplyNode(IntegerLiteralNode(x), IntegerLiteralNode(x)),
+                IntegerLiteralNode(y)),
+              IntegerLiteralNode(z)
+            )
+
+            When("node evaluated")
+            val value: Int = node.eval()
+
+            Then(s"it value should be $res")
+            value should be === res
+          }
+
+        }
+      }
+  }
+
+}
---
+++ interpreter/src/test/scala/azhidkov/ast/IntegerLiteralNodeSpec.scala
@@ -0,0 +1,43 @@
+package azhidkov.ast
+
+import org.scalatest.{Matchers, WordSpec, GivenWhenThen}
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+import org.scalatest.prop.TableDrivenPropertyChecks._
+
+@RunWith(classOf[JUnitRunner])
+class IntegerLiteralNodeSpec extends WordSpec with Matchers with GivenWhenThen {
+
+  val literals =
+    Table("literal",
+      -100,
+      -2,
+      -1,
+      0,
+      1,
+      2,
+      10)
+
+  forAll(literals) {
+    literalValue: Int =>
+    s"An IntegerLiteralNode with value $literalValue" when {
+
+      "evaluated" should {
+
+        "returns it's value" in {
+
+          Given(s"IntegerLiteralNode with value $literalValue")
+          val node = IntegerLiteralNode(literalValue)
+
+          When("node evaluated")
+          val value: Int = node.eval()
+
+          Then(s"it value should be $literalValue")
+          value should be === literalValue
+        }
+
+      }
+    }
+  }
+
+}
